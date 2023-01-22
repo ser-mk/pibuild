2d2af868d5f20b6ea3eabf76fd954cedc86ef1c2
Evgeny Chormonov <en.chormonov@gmail.com>
2023-01-22 21:44:17 +0300

    script update

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ import shutil
 import sys
 
 
-BUILD_DIR = {"android-testdpc", "mclient", "pipi_stadalone"}
+BUILD_DIR = {"admin-dpc", "mclient", "pilauncher"}
 GAME = "piball"
 
 try :
@@ -39,9 +39,9 @@ PATH_CURRENT_TIME_DIR = BUILD_ARCHIVE + "/" + time.strftime("%Y%m%d-%H%M%S-" + T
 os.mkdir(PATH_CURRENT_TIME_DIR)
 
 for dir in BUILD_DIR :
-	print commands.getoutput("cp -v " +dir+ "/app/build/outputs/apk/{0}/*{1}.apk ".format(build_type, build_type) +PATH_CURRENT_TIME_DIR+ "/" + dir + ".apk")
+	print commands.getoutput("cp -v " + dir + "/app/build/outputs/apk/{0}/*{1}.apk ".format(build_type, build_type) + PATH_CURRENT_TIME_DIR + "/" + dir + ".apk")
 
-print commands.getoutput("cp -v "+GAME+"/android/build/outputs/apk/android-{0}.apk ".format(build_type) + PATH_CURRENT_TIME_DIR+ "/"+GAME+".apk")
+print commands.getoutput("cp -v "+ GAME + "/android/build/outputs/apk/android-{0}.apk ".format(build_type) + PATH_CURRENT_TIME_DIR + "/"+ GAME +".apk")
 
 
 PATH_CURRENT_APK_DIR = BUILD_ARCHIVE + "/current"
